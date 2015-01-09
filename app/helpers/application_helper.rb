3aec3a5b26eb983d3cb4762557035bd80c2e8a0d
Shirin Zarghami <shirin.zarghami@gmail.com>
2015-01-09 11:37:10 +0100

    fix a syntax error in category model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ module ApplicationHelper
   def icon_link icon, name, url, link_options = {}
     link_options[:class] ||= ''
     link_options[:class] = link_options[:class] + ' btn btn-link'
+
     link_to url, link_options do
       content_tag(:span, '', class: "glyphicon glyphicon-#{icon}") + name
     end
